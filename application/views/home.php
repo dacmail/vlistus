a9f5974db5e79a4b442e59f928dde9fdfe8f4f4b
Daniel Aguilar <dacmail@gmail.com>
2013-07-03 11:33:06 +0200

    Home styles

--- application/views/home.php
+++ application/views/home.php
@@ -19,13 +19,37 @@
 		</div>
 	</header>
 
-	<section id="create">
+	<section id="create" class="clearfix">
 		<form method="post" action="<?= site_url('playlists/create'); ?>">
 			<label for="name">vlistus.com/</label>
 			<input type="text" id="name" placeholder="playlist name" name="name" />
 			<input type="submit" id="submit" value="Create playlist" />
 		</form>
 	</section>
-
+	<section id="steps" class="clearfix">
+		<aside class="step" id="step-1">
+			<span class="number">1</span>
+			<h2>Create</h2>
+			<p>Lorem ipsum dolor sit amet consequectum dolorem huisquo malver</p>
+		</aside>
+		<aside class="step" id="step-2">
+			<span class="number">2</span>
+			<h2>Share</h2>
+			<p>Lorem ipsum dolor sit amet consequectum dolorem huisquo malver</p>
+		</aside>
+		<aside class="step" id="step-3">
+			<span class="number">3</span>
+			<h2>Play</h2>
+			<p>Lorem ipsum dolor sit amet consequectum dolorem huisquo malver</p>
+		</aside>
+	</section>
+	<footer id="footer" class="clearfix">
+		<div class="wrap">
+		<a href="https://twitter.com/share" class="twitter-share-button" data-via="vlistus">Tweet</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		<iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fvlistus.com&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;font&amp;colorscheme=light&amp;action=like&amp;height=21&amp;appId=468142506573265" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+		<p class="copy">Powered by <a href="http://ungrynerd.com">UNGRYNERD</a></p>
+		</div>
+	</footer>
 </body>
 </html>
